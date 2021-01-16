87203ad90322d4571f356d768f0b0facea92ef18
sajagshrestha <sajagshrestha0852@gmail.com>
2021-01-16 10:14:23 +0545

    login with jwt completed

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,10 +1,12 @@
 import { Response, Request } from "express";
 import User from "../models/user";
-import { registerValidator } from "../validators";
+import { loginValidator, registerValidator } from "../validators";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+//Register a new user
 export const register = async (req: Request, res: Response) => {
-	//check fro validation
+	//check for validation
 	const { error } = registerValidator(req);
 	if (error) {
 		return res.status(422).send(error.details[0].message);
@@ -15,8 +17,8 @@ export const register = async (req: Request, res: Response) => {
 	if (emailExists) return res.status(400).send("email already exists");
 
 	//hash password
-	const salt = await bcrypt.genSalt(10);
-	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
 	//create user
 	const user = new User({
@@ -26,12 +28,29 @@ export const register = async (req: Request, res: Response) => {
 	});
 	try {
 		const savedUser = await user.save();
-		res.status(200).send(`${savedUser.name}'s account has been created`);
+		// res.status(200).send(`${savedUser.name}'s account has been created`);
+		res.send(savedUser);
 	} catch (err) {
 		res.send(err);
 	}
 };
 
-export const login = (req: Request, res: Response) => {
-	res.send("login controller is working");
+//Login
+export const login = async (req: Request, res: Response) => {
+	//check for validation
+	const { error } = loginValidator(req);
+	if (error) {
+		return res.status(422).send(error.details[0].message);
+	}
+	//check email and password
+	const user = await User.findOne({ email: req.body.email });
+	if (!user) return res.status(422).send("Email not found");
+	const passwordMatches = await bcrypt.compare(
+		req.body.password,
+		user.password
+	);
+	if (!passwordMatches) return res.status(422).send("password is incorrect");
+	//return jwt token
+	const token = jwt.sign({ _id: user.id }, process.env.SECRET || "");
+	res.send(token);
 };
